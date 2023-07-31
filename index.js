b366a80c2c8968ccfc6e5a7cd212f0c3752161d5
nuraansa <nuraansa@gmail.com>
2023-07-31 14:02:02 +0200

    fs

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ console.log(path.join(__dirname, 'index.js'));
 const os = require('os');
 console.log(os.platform());
 console.log(os.arch());
+
+//FS
+const
+{writeFile, readFile,
+appendFile,
+read} = require('fs')
+
+readFile('./data.txt', 'utf-8', (err,
+data)=>{
+    if(!err) 
+        console.log(data);
+    else
+        console.log("An error occured");
+})
